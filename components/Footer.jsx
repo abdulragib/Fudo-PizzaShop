7c702b2634cac0f823d8f65be509a9cff10dbcad
Abdul Ragib <abdulragib36@gmail.com>
2023-05-13 14:16:10 +0530

    Footer -> Footer done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,24 @@
+import {UilGithub} from '@iconscout/react-unicons'
+import {UilFacebook} from '@iconscout/react-unicons'
+import {UilInstagram} from '@iconscout/react-unicons'
+import css from '../styles/footer.module.css'
+import Image from 'next/image'
+import Logo from '../assets/Logo.png'
+
 export default function Footer (params) {
    return(
-      <div>
-          <h1>Footer</h1>
+      <div className={css.container}>
+          <span>ALL RIGHT RESERVED</span>
+          <div className={css.social}>
+                <UilFacebook size={45} color="#2E2E2E"/>
+                <UilGithub size={45} color="#2E2E2E"/>
+                <UilInstagram size={45} color="#2E2E2E"/>
+          </div>
+
+          <div className={css.logo}>
+            <Image src={Logo} alt="" width={50} height={50}/>
+            <span>Fudo</span>
+        </div>
       </div>
    )
 }
