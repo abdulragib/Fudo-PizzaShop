65f6d11aa4d3170e457e809e7a278ec7c3a9395e
Abdul Ragib <abdulragib36@gmail.com>
2023-05-23 23:58:44 +0530

    Drawer setup in header

--- components/Drawer.jsx
+++ components/Drawer.jsx
@@ -3,28 +3,29 @@ import Drawer from '@mui/material/Drawer';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import {IconButton} from "@mui/material";
 import {Link} from "react-router-dom";
+import css from '../styles/drawer.module.css'
 
 export default function TemporaryDrawer() {
    const [open, setOpen] = useState(false);
     return (
         <div>
                     <IconButton onClick={()=>setOpen(true)}>
-                        <MenuRoundedIcon className="link"/>
+                        <MenuRoundedIcon className={css.link}/>
                     </IconButton>
                     <Drawer
                         anchor={"right"}
                         open={open}
                         onClose={()=>setOpen(false)}
                     >
-                        <div className="drawer-div">
+                        <div className={css.drawer_div}>
                             <Link to="../">
-                                <p className="link">Home</p>
+                                <p className={css.link}>Home</p>
                             </Link>
-                            <Link to="/compare">
-                                <p className="link">Menu</p>
+                            <Link to="/menu">
+                                <p className={css.link}>Menu</p>
                             </Link>
                             <Link to="/contact">
-                                <p className="link">Contact</p>
+                                <p className={css.link}>Contact</p>
                             </Link>
                         </div>
                     </Drawer>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,21 @@
 import css from "../styles/header.module.css";
 import Image from "next/image";
 import Logo from "../assets/Logo.png";
+import Link from "next/link";
 import { UilShoppingBag, UilReceipt } from "@iconscout/react-unicons";
 import { useStore } from "../store/store";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import TemporaryDrawer from "./Drawer";
+import dynamic from 'next/dynamic';
 
-export default function Header() {
+const Header = () => {
   const [Order, setOrder] = useState("");
-
   useEffect(() => {
     setOrder(localStorage.getItem("order"));
   }, []);
 
   const items = useStore((state) => state.cart.pizzas.length);
+
   return (
     <div className={css.header}>
       {/* logo side */}
@@ -28,8 +29,12 @@ export default function Header() {
         <li>
           <Link href="../">Home</Link>
         </li>
-        <li><Link href="#">Menu</Link></li>
-        <li><Link href="#">Contact</Link></li>
+        <li>
+          <Link href="#">Menu</Link>
+        </li>
+        <li>
+          <Link href="#">Contact</Link>
+        </li>
       </ul>
 
       {/* right side */}
@@ -45,15 +50,30 @@ export default function Header() {
           <Link href={`/order/${Order}`}>
             <div className={css.cart}>
               <UilReceipt size={35} color="#2E2E2E" />
-              {Order != "" && <div className={css.badge}>1</div>}
+              {Order !== "" && <div className={css.badge}>1</div>}
             </div>
           </Link>
         )}
       </div>
 
-      <div className="mobile-drawer">
-            <TemporaryDrawer/>
+      <div className={css.mobile_drawer}>
+        <TemporaryDrawer />
       </div>
     </div>
   );
-}
+};
+
+// Load BrowserRouter component on the client-side using dynamic import
+const DynamicBrowserRouter = dynamic(() => import('react-router-dom').then(module => module.BrowserRouter), {
+  ssr: false // Set ssr to false to only load on the client-side
+});
+
+const App = () => {
+  return (
+    <DynamicBrowserRouter>
+      <Header />
+    </DynamicBrowserRouter>
+  );
+};
+
+export default App;
